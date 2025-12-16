4fedf2e3b822c0e2fef791715d2230a9993da7af
Mohamed Farhan <pmdfarhan@gmail.com>
2025-12-16 22:23:41 +0530

    added hero section and modified dark mode switch transition animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,8 +27,29 @@ const Main = () => {
 
   return (
     <>
+      <div
+        className="flex flex-col max-w-300 items-center justify-center mx-auto gap-4 sm:gap-4
+       py-6 px-5 mt-4 sm:mb-2 sm:mt-8"
+      >
+        <h1 className="text-4xl dark:text-white font-bold text-center">
+          Discover your next{" "}
+          <span
+            className="bg-linear-to-r from-red-700 via-red-600 to-rose-600
+            dark:from-yellow-400 dark:via-amber-500 dark:to-yellow-600
+            bg-clip-text text-transparent
+"
+          >
+            Favourite Movie
+          </span>
+        </h1>
+        <h3 className="dark:text-white text-md sm:text-xl dark:font-light  text-center">
+          Search to find the perfect film for any mood. Find the film that
+          matches your vibe. Grabbed your popcorn?{" "}
+          <span className="text-xl">🍿</span>
+        </h3>
+      </div>
       <Search setSearch={setSearch} search={search} onSearch={handleClick} />
-      <div className="main px-4 pt-10 gap-6 sm:px-0 sm:gap-8 sm:pt-20 pb-15 flex flex-wrap sm:max-w-300 sm:mx-auto">
+      <div className="main px-4 pt-10 gap-6 sm:px-0 sm:gap-8 sm:pt-16 pb-15 flex flex-wrap sm:max-w-300 sm:mx-auto">
         {data && data?.Search?.length > 0
           ? data?.Search?.map((movie) => (
               <Card key={movie?.imdbID} movie={movie} />
